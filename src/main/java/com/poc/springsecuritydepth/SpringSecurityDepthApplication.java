ef4b87fd161534c2362cf7265dcbfaff54bc13b2
Anup Gupta <agupta-contractor@ambrygen.com>
2025-08-07 21:55:31 +0530

    Initialize Spring Security Depth project with Maven setup and basic application structure

---
+++ src/main/java/com/poc/springsecuritydepth/SpringSecurityDepthApplication.java
@@ -0,0 +1,13 @@
+package com.poc.springsecuritydepth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDepthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityDepthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poc/springsecuritydepth/SpringSecurityDepthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.springsecuritydepth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDepthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
